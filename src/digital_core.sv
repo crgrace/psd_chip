618f74f75ba29bf70292673d7002fc6af13e2b43
dgnani <DGnani@lbl.gov>
2021-02-18 13:07:33 -0800

    added updated defaults

--- src/digital_core.sv
+++ src/digital_core.sv
@@ -4,7 +4,7 @@
 // Description: PSD_CHIP synthesized digital core.  
 //              Includes:
 //              UARTs for inter-chip communcation
-//              32-byte Register Map for configuration bits.
+//              42-byte Register Map for configuration bits.
 //
 //              Note that the "primary" is the chip writing to and reading
 //              from the current chip. It could also be the FPGA.
@@ -13,7 +13,7 @@
 ///////////////////////////////////////////////////////////////////
 
 module digital_core
-    #(parameter NUMREGS = 32)   // number of configuration registers
+    #(parameter NUMREGS = 42)   // number of configuration registers
     
     (output logic piso,// PRIMARY-IN-SECONDARY-OUT TX UART output bit
     
@@ -21,45 +21,57 @@ module digital_core
 // these are in the same order as the PSD_CHIP config bits google sheet
     output logic [3:0] disable_channel, // high to power down channel
     output logic [3:0] enable_replica, // high to use replica circuits
-    output logic [7:0] v_buff_ref_global, // global ref for both int stages
+    output logic [2:0] v_buff_ref_global, // global ref for both int stages
     output logic [7:0] v_buff_ref_fine0, // fine ref for both int stages
     output logic [7:0] v_buff_ref_fine1, // fine ref for both int stages
     output logic [7:0] v_buff_ref_fine2, // fine ref for both int stages
     output logic [7:0] v_buff_ref_fine3, // fine ref for both int stages
+    output logic [7:0] v_fine_refp, // positive DAC reference 
+    output logic [7:0] v_fine_refn, // negative DAC reference 
     output logic [7:0] v_ref_comp0, // opamp ref voltage
     output logic [7:0] v_ref_comp1, // opamp ref voltage
     output logic [7:0] v_ref_comp2, // opamp ref voltage
     output logic [7:0] v_ref_comp3, // opamp ref voltage
-    output logic [7:0] v_comp_thresh_sout_global, // SOUT disc. thresh
+    output logic [2:0] v_comp_thresh_sout_global, // SOUT disc. thresh
     output logic [7:0] v_comp_thresh_sout_fine0, // SOUT disc. thresh
     output logic [7:0] v_comp_thresh_sout_fine1, // SOUT disc. thresh
     output logic [7:0] v_comp_thresh_sout_fine2, // SOUT disc. thresh
     output logic [7:0] v_comp_thresh_sout_fine3, // SOUT disc. thresh
-    output logic [7:0] v_comp_thresh_fout_global, // FOUT disc. thresh
+    output logic [2:0] v_comp_thresh_fout_global, // FOUT disc. thresh
     output logic [7:0] v_comp_thresh_fout_fine0, // FOUT disc. thresh
     output logic [7:0] v_comp_thresh_fout_fine1, // FOUT disc. thresh
     output logic [7:0] v_comp_thresh_fout_fine2, // FOUT disc. thresh
     output logic [7:0] v_comp_thresh_fout_fine3, // FOUT disc. thresh
-    output logic [3:0] i_bias_total_int, // delay bias for total int
-    output logic [3:0] i_bias_partial_int, // delay bias for partial int
-    output logic [3:0] i_bias_disc, // bias current for discriminator
-    output logic [3:0] i_bias_disc_sout_delay, // delay for disc output
-    output logic [3:0] i_bias_fout_width_trim, // std width trim
-    output logic [3:0] i_bias_lvds, // sets lvds loop current
+    output logic [4:0] i_bias_total_int, // delay bias for total int
+    output logic [4:0] i_bias_partial_int, // delay bias for partial int
+    output logic [4:0] i_bias_hold, // bias for hold line width
+    output logic [4:0] i_bias_hold_delay, // bias for hold line delay
+    output logic [4:0] i_bias_disc_sout_delay, // delay for disc output
+    output logic [4:0] i_bias_fout_width_trim, // std width trim
+    output logic [4:0] i_bias_sout_comp_width, // std width trim
     output logic sel_std_sout_comp_width, // high for std width
     output logic [7:0] tunable_res_total_int, // adjust resistor value
-    output logic [7:0] tunable_res_subtr_gain, // adjust resistor value
-    output logic [2:0] digital_testbus0_sel, // select signal to testbus0
-    output logic digital_testbus0_en, // enable testbus0
-    output logic [2:0] digital_testbus1_sel, // select signal to testbus1
-    output logic digital_testbus1_en, // enable testbus1
-    output logic [3:0] external_trigger_enable, // high to en ext trig
-    output logic [3:0] cross_trigger_enable, // high to en cross trig
+    output logic [6:0] tunable_res_msbs, // adjust resistor value
+    output logic [5:0] tunable_res_subtr_gain, // adjust resistor value
+    output logic [4:0] digital_testbus0_sel, // select signal to testbus0
+    output logic digital_testbus0_pd, // pd testbus0
+    output logic [4:0] digital_testbus1_sel, // select signal to testbus1
+    output logic digital_testbus1_pd, // pd testbus1
+    output logic [4:0] digital_testbus2_sel, // select signal to testbus2
+    output logic digital_testbus2_pd, // pd testbus2
+    output logic [4:0] digital_testbus3_sel, // select signal to testbus3
+    output logic digital_testbus3_pd, // pd testbus3
+    output logic [3:0] sout_comp_out_pd, // high to power down LVDS chan
+    output logic lvds_rx_pd, // shared pd for LVDS inputs
+    output logic lvds_high_cm, // use high LVDS cm if asserted
+    output logic lvds_loopback_enable, // high to enable loopback
+    output logic external_trigger_enable, // high to en ext trig
+    output logic cross_trigger_enable, // high to en cross trig
     output logic [7:0] current_monitor, // measure internal currents
     output logic [7:0] voltage_monitor, // measure internal voltages
 // SPARES
-    output logic [3:0] spare0,  // 4 spare configuration bits
-    output logic [3:0] spare1,  // 4 spare configuration bits
+    output logic [7:0] spare0,  // 8 spare configuration bits
+    output logic [7:0] spare1,  // 8 spare configuration bits
 // INPUTS
     input logic posi,       // PRIMARY-OUT-SECONDARY-IN: RX UART input bit  
     input logic clk,        // clk for UART
@@ -71,45 +83,56 @@ logic [7:0] config_bits [0:NUMREGS-1];// regmap config bit outputs
 always_comb begin
     disable_channel = config_bits[CHANNEL_DISABLE][3:0];
     enable_replica = config_bits[CHANNEL_DISABLE][7:4];
-    v_buff_ref_global = config_bits[BUFF_REF0][7:0];
+    v_buff_ref_global = config_bits[BUFF_REF0][2:0];
     v_buff_ref_fine0 = config_bits[BUFF_REF1][7:0];
     v_buff_ref_fine1 = config_bits[BUFF_REF2][7:0];
     v_buff_ref_fine2 = config_bits[BUFF_REF3][7:0];
     v_buff_ref_fine3 = config_bits[BUFF_REF4][7:0];
+    v_fine_refp = config_bits[FINE_REFP][7:0];
+    v_fine_refn = config_bits[FINE_REFN][7:0];
     v_ref_comp0 = config_bits[D2S_REF0][7:0];
     v_ref_comp1 = config_bits[D2S_REF1][7:0];
     v_ref_comp2 = config_bits[D2S_REF2][7:0];
     v_ref_comp3 = config_bits[D2S_REF3][7:0];
-    v_comp_thresh_sout_global = config_bits[THRESH_SOUT_GLOBAL][7:0];
+    v_comp_thresh_sout_global = config_bits[THRESH_SOUT_GLOBAL][2:0];
     v_comp_thresh_sout_fine0 = config_bits[THRESH_SOUT_FINE0][7:0];
     v_comp_thresh_sout_fine1 = config_bits[THRESH_SOUT_FINE1][7:0];
     v_comp_thresh_sout_fine2 = config_bits[THRESH_SOUT_FINE2][7:0];
     v_comp_thresh_sout_fine3 = config_bits[THRESH_SOUT_FINE3][7:0];
-    v_comp_thresh_fout_global = config_bits[THRESH_FOUT_GLOBAL][7:0];
+    v_comp_thresh_fout_global = config_bits[THRESH_FOUT_GLOBAL][2:0];
     v_comp_thresh_fout_fine0 = config_bits[THRESH_FOUT_FINE0][7:0];
     v_comp_thresh_fout_fine1 = config_bits[THRESH_FOUT_FINE1][7:0];
     v_comp_thresh_fout_fine2 = config_bits[THRESH_FOUT_FINE2][7:0];
     v_comp_thresh_fout_fine3 = config_bits[THRESH_FOUT_FINE3][7:0];
-    i_bias_total_int = config_bits[IBIAS_TOTAL_INT][3:0];
-    spare0 = config_bits[IBIAS_TOTAL_INT][7:4];
-    i_bias_partial_int = config_bits[IBIAS_PARTIAL_INT][3:0];
-    spare1 = config_bits[IBIAS_PARTIAL_INT][3:0];
-    i_bias_disc = config_bits[IBIAS_DISC][3:0];
-    i_bias_disc_sout_delay = config_bits[IBIAS_DISC_OUT][3:0];
-    i_bias_fout_width_trim = config_bits[IBIAS_FOUT_WIDTH][3:0];
-    sel_std_sout_comp_width = config_bits[IBIAS_FOUT_WIDTH][4];
-    i_bias_lvds = config_bits[IBIAS_LVDS][3:0];
+    i_bias_total_int = config_bits[IBIAS_TOTAL_INT][4:0];
+    i_bias_partial_int = config_bits[IBIAS_PARTIAL_INT][4:0];
+    i_bias_hold = config_bits[IBIAS_HOLD][4:0];
+    i_bias_hold_delay = config_bits[IBIAS_HOLD_DELAY][4:0];
+    i_bias_disc_sout_delay = config_bits[IBIAS_SOUT_DELAY][4:0];
+    i_bias_fout_width_trim = config_bits[IBIAS_FOUT_WIDTH][4:0];
+    i_bias_sout_comp_width = config_bits[IBIAS_SOUT_COMP_WIDTH][4:0];
+    sel_std_sout_comp_width = config_bits[IBIAS_SOUT_COMP_WIDTH][4];
     tunable_res_total_int = config_bits[TUNABLE_RES_TOTAL_INT][7:0];
-    tunable_res_subtr_gain = config_bits[TUNABLE_RES_SUBTR_GAIN][7:0];
-    digital_testbus0_sel = config_bits[DIGITAL_TESTBUS][2:0];
-    digital_testbus0_en = config_bits[DIGITAL_TESTBUS][3];
-    digital_testbus1_sel = config_bits[DIGITAL_TESTBUS][6:4];
-    digital_testbus1_en = config_bits[DIGITAL_TESTBUS][7];
-    external_trigger_enable = config_bits[TRIGGER][3:0];
-    cross_trigger_enable = config_bits[TRIGGER][7:4];
+    tunable_res_msbs = config_bits[TUNABLE_RES_TOTAL_INT+1][6:0];
+    tunable_res_subtr_gain = config_bits[TUNABLE_RES_SUBTR_GAIN][5:0];
+    digital_testbus0_sel = config_bits[DIGITAL_TESTBUS0][4:0];
+    digital_testbus0_pd = config_bits[DIGITAL_TESTBUS0][5];
+    digital_testbus1_sel = config_bits[DIGITAL_TESTBUS1][4:0];
+    digital_testbus1_pd = config_bits[DIGITAL_TESTBUS1][5];
+    digital_testbus2_sel = config_bits[DIGITAL_TESTBUS2][4:0];
+    digital_testbus2_pd = config_bits[DIGITAL_TESTBUS2][5];
+    digital_testbus3_sel = config_bits[DIGITAL_TESTBUS3][4:0];
+    digital_testbus3_pd = config_bits[DIGITAL_TESTBUS3][5];
+    sout_comp_out_pd = config_bits[LVDS][3:0];
+    lvds_rx_pd = config_bits[LVDS][4];
+    lvds_high_cm = config_bits[LVDS][5];
+    lvds_loopback_enable = config_bits[LVDS][6];
+    external_trigger_enable = config_bits[TRIGGER][0];
+    cross_trigger_enable = config_bits[TRIGGER][1];
     current_monitor = config_bits[IMONITOR][7:0];
     voltage_monitor = config_bits[VMONITOR][7:0];
-
+    spare0 = config_bits[SPARE0][7:0];
+    spare1 = config_bits[SPARE1][7:0];
 end // always_comb
 
 
--- testbench/unit_tests/digital_core_tb.sv
+++ testbench/unit_tests/digital_core_tb.sv
@@ -16,50 +16,62 @@
 
 module digital_core_tb();
     timeunit 1ns/10ps;
-    localparam NUMREGS = 32;
+    localparam NUMREGS = 42;
 
 // config bits
 
 logic [3:0] disable_channel; // high to power down channel
 logic [3:0] enable_replica; // high to use replica circuits
-logic [7:0] v_buff_ref_global; // global ref for both int stages
+logic [2:0] v_buff_ref_global; // global ref for both int stages
 logic [7:0] v_buff_ref_fine0; // fine ref for both int stages
 logic [7:0] v_buff_ref_fine1; // fine ref for both int stages
 logic [7:0] v_buff_ref_fine2; // fine ref for both int stages
 logic [7:0] v_buff_ref_fine3; // fine ref for both int stages
+logic [7:0] v_fine_refp; // positive DAC reference 
+logic [7:0] v_fine_refn; // negative DAC reference 
 logic [7:0] v_ref_comp0; // opamp ref voltage
 logic [7:0] v_ref_comp1; // opamp ref voltage
 logic [7:0] v_ref_comp2; // opamp ref voltage
 logic [7:0] v_ref_comp3; // opamp ref voltage
-logic [7:0] v_comp_thresh_sout_global; // SOUT disc. thresh
+logic [2:0] v_comp_thresh_sout_global; // SOUT disc. thresh
 logic [7:0] v_comp_thresh_sout_fine0; // SOUT disc. thresh
 logic [7:0] v_comp_thresh_sout_fine1; // SOUT disc. thresh
 logic [7:0] v_comp_thresh_sout_fine2; // SOUT disc. thresh
 logic [7:0] v_comp_thresh_sout_fine3; // SOUT disc. thresh
-logic [7:0] v_comp_thresh_fout_global; // FOUT disc. thresh
+logic [2:0] v_comp_thresh_fout_global; // FOUT disc. thresh
 logic [7:0] v_comp_thresh_fout_fine0; // FOUT disc. thresh
 logic [7:0] v_comp_thresh_fout_fine1; // FOUT disc. thresh
 logic [7:0] v_comp_thresh_fout_fine2; // FOUT disc. thresh
 logic [7:0] v_comp_thresh_fout_fine3; // FOUT disc. thresh
-logic [3:0] i_bias_total_int; // delay bias for total int
-logic [3:0] i_bias_partial_int; // delay bias for partial int
-logic [3:0] i_bias_disc; // bias current for discriminator
-logic [3:0] i_bias_disc_sout_delay; // delay for disc output
-logic [3:0] i_bias_fout_width_trim; // std width trim
+logic [4:0] i_bias_total_int; // delay bias for total int
+logic [4:0] i_bias_partial_int; // delay bias for partial int
+logic [4:0] i_bias_hold; //  bias for hold line width
+logic [4:0] i_bias_hold_delay; // bias for hold line delay
+logic [4:0] i_bias_disc_sout_delay; // delay for disc output
+logic [4:0] i_bias_fout_width_trim; // std width trim
+logic [4:0] i_bias_sout_comp_width; // std width trim
 logic sel_std_sout_comp_width; // high for std width
-logic [3:0] i_bias_lvds; // sets lvds loop current
 logic [7:0] tunable_res_total_int; // adjust resistor value
-logic [7:0] tunable_res_subtr_gain; // adjust resistor value
-logic [2:0] digital_testbus0_sel; // select signal to testbus0
-logic digital_testbus0_en; // enable testbus0
-logic [2:0] digital_testbus1_sel; // select signal to testbus1
-logic digital_testbus1_en; // enable testbus1
-logic [3:0] external_trigger_enable; // high to en ext trig
-logic [3:0] cross_trigger_enable; // high to en cross trig
+logic [6:0] tunable_res_msbs; // adjust resistor value
+logic [5:0] tunable_res_subtr_gain; // adjust resistor value
+logic [4:0] digital_testbus0_sel; // select signal to testbus0
+logic digital_testbus0_pd; // powerdown testbus0
+logic [4:0] digital_testbus1_sel; // select signal to testbus1
+logic digital_testbus1_pd; // powerdown testbus1
+logic [4:0] digital_testbus2_sel; // select signal to testbus2
+logic digital_testbus2_pd; // powerdown testbus2
+logic [4:0] digital_testbus3_sel; // select signal to testbus3
+logic digital_testbus3_pd; // powerdown testbus3
+logic [3:0] sout_comp_out_pd; // high to power down LVDS chan
+logic lvds_rx_pd; // shared pd for LVDS inputs
+logic lvds_high_cm; // use high LVDS cm if asserted
+logic lvds_loopback_enable; // high to enable loopback
+logic external_trigger_enable; // high to en ext trig
+logic cross_trigger_enable; // high to en cross trig
 logic [7:0] current_monitor; // measure internal currents
 logic [7:0] voltage_monitor; // measure internal voltages
-logic [3:0] spare0; // 4 spare config bits
-logic [3:0] spare1; // 4 spare config bits
+logic [7:0] spare0; // 8 spare config bits
+logic [7:0] spare1; // 8 spare config bits
 
 
 // local digital_signals
@@ -118,7 +130,7 @@ initial begin
 
 //#2000 regfileOpUART(READ,8'h01,8'h00);
 //    testExternalInterfaceUART(8'hab);
-#1000      randomTestExternalInterface(100);
+#1000      randomTestExternalInterface(10000);
 
 // test RX rejection of run start bit
     #1000 clk_en = 1;
@@ -193,6 +205,8 @@ digital_core
     .v_buff_ref_fine1           (v_buff_ref_fine1),
     .v_buff_ref_fine2           (v_buff_ref_fine2),
     .v_buff_ref_fine3           (v_buff_ref_fine3),
+    .v_fine_refp                (v_fine_refp),
+    .v_fine_refn                (v_fine_refn),
     .v_ref_comp0                (v_ref_comp0),
     .v_ref_comp1                (v_ref_comp1),
     .v_ref_comp2                (v_ref_comp2),
@@ -209,17 +223,27 @@ digital_core
     .v_comp_thresh_fout_fine3   (v_comp_thresh_fout_fine3),
     .i_bias_total_int           (i_bias_total_int),
     .i_bias_partial_int         (i_bias_partial_int),
-    .i_bias_disc                (i_bias_disc),
+    .i_bias_hold                (i_bias_hold),
+    .i_bias_hold_delay          (i_bias_hold_delay),
     .i_bias_disc_sout_delay     (i_bias_disc_sout_delay),
     .i_bias_fout_width_trim     (i_bias_fout_width_trim),
+    .i_bias_sout_comp_width     (i_bias_sout_comp_width),
     .sel_std_sout_comp_width    (sel_std_sout_comp_width),
-    .i_bias_lvds                (i_bias_lvds),
     .tunable_res_total_int      (tunable_res_total_int),
+    .tunable_res_msbs           (tunable_res_msbs),
     .tunable_res_subtr_gain     (tunable_res_subtr_gain),
     .digital_testbus0_sel       (digital_testbus0_sel),
-    .digital_testbus0_en        (digital_testbus0_en),
+    .digital_testbus0_pd        (digital_testbus0_pd),
     .digital_testbus1_sel       (digital_testbus1_sel),
-    .digital_testbus1_en        (digital_testbus1_en),
+    .digital_testbus1_pd        (digital_testbus1_pd),
+    .digital_testbus2_sel       (digital_testbus2_sel),
+    .digital_testbus2_pd        (digital_testbus2_pd),
+    .digital_testbus3_sel       (digital_testbus3_sel),
+    .digital_testbus3_pd        (digital_testbus3_pd),
+    .sout_comp_out_pd           (sout_comp_out_pd),
+    .lvds_rx_pd                 (lvds_rx_pd),
+    .lvds_high_cm               (lvds_high_cm),
+    .lvds_loopback_enable       (lvds_loopback_enable),
     .external_trigger_enable    (external_trigger_enable),
     .cross_trigger_enable       (cross_trigger_enable),
     .current_monitor            (current_monitor),
